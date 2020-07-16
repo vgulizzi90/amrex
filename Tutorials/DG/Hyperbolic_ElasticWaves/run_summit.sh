042e306f36466159d6a0bce5d28c526f69b316ef
vgulizzi <vgulizzi@summit.olcf.ornl.gov>
2020-07-16 11:05:52 -0400

    Working on 3d now.

--- Tutorials/DG/Hyperbolic_ElasticWaves/run_summit.sh
+++ Tutorials/DG/Hyperbolic_ElasticWaves/run_summit.sh
@@ -2,22 +2,23 @@
 
 #BSUB -P CSC308
 #BSUB -J ICS_periodic_BCS_periodic
-#BSUB -W 1:00
+#BSUB -W 2:00
 #BSUB -nnodes 1
 
-RACE_PATH="RACE/Projects/2020/amrex/Tutorials/DG/Hyperbolic_ElasticWaves/"
+N_MPI_RANKS="1"
+RACE_PATH="RACE/Projects/2020/amrex/Tutorials/DG/Hyperbolic_ElasticWaves"
 HOME_DIR="$HOME/$RACE_PATH"
-WORK_DIR="$MEMBERWORK/csc308/$RACE_PATH"
+WORK_DIR="$MEMBERWORK/csc308/$RACE_PATH/n_MPI_ranks_$N_MPI_RANKS"
 
 INPUT="inputs"
-EXE="./main2d.gnu.MPI.ex"
+EXE="./main3d.gnu.MPI.ex"
 
 cp $INPUT $WORK_DIR
 cp $EXE $WORK_DIR
 cd $WORK_DIR
 
 date
-jsrun --nrs 1 -a 4 -c 4 $EXE $INPUT
+jsrun --nrs 1 -a 8 -c 8 $EXE $INPUT
 
 
 
