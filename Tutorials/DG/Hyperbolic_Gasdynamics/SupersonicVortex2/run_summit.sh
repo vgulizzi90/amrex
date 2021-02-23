14726fc191f48e7f567b03df68935209420d6c90
vgulizzi <vgulizzi@lbl.gov>
2021-02-23 11:46:35 -0800

    Working on the tilted Sod's tube test.

---
+++ Tutorials/DG/Hyperbolic_Gasdynamics/SupersonicVortex2/run_summit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#BSUB -P CSC308
+#BSUB -J m256x256
+#BSUB -W 2:00
+#BSUB -nnodes 1
+
+RACE_PATH="RACE/Projects/2020/amrex/Tutorials/DG/Hyperbolic_Gasdynamics/SupersonicVortex2"
+HOME_DIR="$HOME/$RACE_PATH"
+WORK_DIR="$MEMBERWORK/csc308/$RACE_PATH"
+
+INPUT="inputs"
+EXE="./main2d.gnu.MPI.ex"
+
+cp $INPUT $WORK_DIR
+cp $EXE $WORK_DIR
+cd $WORK_DIR
+
+date
+jsrun --nrs 1 -a 42 -c 42 $EXE $INPUT
